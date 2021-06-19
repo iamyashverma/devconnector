2dab1dc836327dd2ef0bebcfaca6f69ddd04b138
Yash Verma <yash.verma24071990@gmail.com>
2021-06-20 02:07:32 +0530

    Setup redux store

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -5,21 +5,25 @@ import { Landing } from './components/layout/Landing';
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
   <div className='App'>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   </div>
 );
 
