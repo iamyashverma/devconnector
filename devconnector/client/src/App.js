4ba58f4cbebbd2b5c5231589db3df2a3999f517e
Yash Verma <yash.verma24071990@gmail.com>
2021-06-20 01:46:21 +0530

    Added Login and Register structure

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,11 +1,25 @@
 import { Fragment } from 'react';
 import './App.css';
+import { Navbar } from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div className='App'>
-    <Fragment>
-      <h1>APP</h1>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   </div>
 );
 
